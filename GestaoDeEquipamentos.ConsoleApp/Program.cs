d437a5e7a87c827687cb2e71b7ac376c6aac30cb
Igor Mota <igormello399@gmail.com>
2026-04-09 14:52:01 -0300

    Adicionado parte de cadastro de equipamentos

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Equipamento[]? equipamentos = new Equipamento[10];
         while (true)
         {
             Console.Clear();
@@ -27,6 +28,57 @@ class Program
 
             if (opcaoMenu == "1")
             {
+                Console.Clear();
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Gestão de Equipamentos");
+                Console.WriteLine("---------------------------------");
+                System.Console.WriteLine("Cadastro de equipamento");
+                System.Console.WriteLine("---------------------------");
+
+                Equipamento novoEquipamento = new Equipamento();
+
+                do
+                {
+                    System.Console.Write("Digite o nome do equipamento");
+                    novoEquipamento.nome = Console.ReadLine();
+
+                    if (!string.IsNullOrWhiteSpace(novoEquipamento.nome) && novoEquipamento.nome.Length > 3)
+                    {
+                        break;
+                    }
+                } while (true);
+
+                do
+                {
+                    System.Console.Write("Digite o fabricante");
+                    novoEquipamento.fabricante = Console.ReadLine();
+
+                    if (!string.IsNullOrWhiteSpace(novoEquipamento.fabricante) && novoEquipamento.fabricante.Length > 3)
+                    {
+                        break;
+                    }
+                } while (true);
+
+                System.Console.Write("Digite o preço de aquisição do equipamento:");
+                novoEquipamento.precoDeAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+                System.Console.Write("Digite a data de fabricação do equipamento:");
+                novoEquipamento.dataDeFabricacao = Convert.ToDateTime(Console.ReadLine());
+
+                for (int i = 0; i < equipamentos.Length; i++)
+                {
+                    Equipamento? e = equipamentos[i];
+
+                    if (e == null)
+                    {
+                        equipamentos[i] = novoEquipamento;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine($"O registro \"{novoEquipamento.nome}\" foi cadastrado com sucesso");
+                Console.WriteLine("---------------------------------");
 
             }
 
