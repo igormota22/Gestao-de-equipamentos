88f4d19b77bfd4373a050e0dc30e36a2cfdad8d9
Igor Mota <igormello399@gmail.com>
2026-04-09 15:39:26 -0300

    Adicionado id do equipamento e criada tabela para visualizar

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp;
+﻿using System.Security.Cryptography;
+
+namespace GestaoDeEquipamentos.ConsoleApp;
 
 class Program
 {
@@ -39,7 +41,7 @@ class Program
 
                 do
                 {
-                    System.Console.Write("Digite o nome do equipamento");
+                    System.Console.Write("Digite o nome do equipamento:");
                     novoEquipamento.nome = Console.ReadLine();
 
                     if (!string.IsNullOrWhiteSpace(novoEquipamento.nome) && novoEquipamento.nome.Length > 3)
@@ -50,7 +52,7 @@ class Program
 
                 do
                 {
-                    System.Console.Write("Digite o fabricante");
+                    System.Console.Write("Digite o fabricante:");
                     novoEquipamento.fabricante = Console.ReadLine();
 
                     if (!string.IsNullOrWhiteSpace(novoEquipamento.fabricante) && novoEquipamento.fabricante.Length > 3)
@@ -65,6 +67,8 @@ class Program
                 System.Console.Write("Digite a data de fabricação do equipamento:");
                 novoEquipamento.dataDeFabricacao = Convert.ToDateTime(Console.ReadLine());
 
+                novoEquipamento.id = Convert.ToHexString(RandomNumberGenerator.GetBytes(20)).ToLower().Substring(0, 7);
+
                 for (int i = 0; i < equipamentos.Length; i++)
                 {
                     Equipamento? e = equipamentos[i];
@@ -77,13 +81,42 @@ class Program
                 }
 
                 Console.WriteLine("---------------------------------");
-                Console.WriteLine($"O registro \"{novoEquipamento.nome}\" foi cadastrado com sucesso");
+                Console.WriteLine($"O registro \"{novoEquipamento.nome}\" foi cadastrado com sucesso!Digite ENTER para continuar");
                 Console.WriteLine("---------------------------------");
+                Console.ReadLine();
 
             }
 
             else if (opcaoMenu == "2")
             {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Ediçao de Equipamento");
+                Console.WriteLine("---------------------------------");
+
+                Console.WriteLine(
+           "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+           "Id", "Nome", "Fabricante", "Preço de Aquisição", "Data de Fabricação"
+       );
+
+                for (int i = 0; i < equipamentos.Length; i++)
+                {
+                    Equipamento? e = equipamentos[i];
+
+                    if (e == null)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(
+              "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+              e.id, e.nome, e.fabricante, e.precoDeAquisicao.ToString("C2"), e.dataDeFabricacao.ToShortDateString()
+                );
+                    System.Console.WriteLine();
+
+                    System.Console.WriteLine("Digite ENTER para continuar");
+                    Console.ReadLine();
+                }
+
+                System.Console.Write("Informe o ID do equipamento que deseja atualizar:");
 
             }
 
