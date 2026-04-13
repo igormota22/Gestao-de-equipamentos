22dda3b7665c4a2e1bbc8ed4d17d46ab0ca36178
Igor Mota <igormello399@gmail.com>
2026-04-13 14:59:54 -0300

    Implementados os metodos da classe RepositorioFabricante

--- GestaoDeEquipamentos.ConsoleApp/Interface/RepositorioFabricante.cs
+++ GestaoDeEquipamentos.ConsoleApp/Interface/RepositorioFabricante.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 using GestaoDeEquipamentos.ConsoleApp.Dominio;
 
 namespace GestaoDeEquipamentos.ConsoleApp.Interface;
@@ -9,7 +10,18 @@ public class RepositorioFabricante
 
     public void Cadastrar(Fabricante novoFabricante)
     {
+        novoFabricante.id = Convert.ToHexString(RandomNumberGenerator.GetBytes(20)).ToLower().Substring(0, 7);
 
+        for (int i = 0; i < fabricantes.Length; i++)
+        {
+            Fabricante? f = fabricantes[i];
+
+            if (f == null)
+            {
+                fabricantes[i] = novoFabricante;
+                break;
+            }
+        }
     }
 
     public Fabricante?[] SelecionarTodos()
@@ -21,19 +33,58 @@ public class RepositorioFabricante
     {
         Fabricante? fabricanteSelecionado = null;
 
+        for (int i = 0; i < fabricantes.Length; i++)
+        {
+            Fabricante? f = fabricantes[i];
+
+            if (f == null)
+                continue;
+
+            if (f.id == idSelecionado)
+            {
+                fabricanteSelecionado = f;
+                break;
+            }
+        }
 
         return fabricanteSelecionado;
     }
 
     public bool Editar(string idSelecionado, Fabricante novoFabricante)
     {
+        Fabricante? fabricanteSelecionado = SelecionarPorID(idSelecionado);
 
+        if (fabricanteSelecionado == null)
+        {
+            return false;
+        }
+
+        fabricanteSelecionado?.nome = novoFabricante.nome;
+        fabricanteSelecionado?.email = novoFabricante.email;
+        fabricanteSelecionado?.telefone = novoFabricante.telefone;
 
         return true;
     }
 
     public bool Excluir(string idSelecionado)
     {
+        for (int i = 0; i < fabricantes.Length; i++)
+        {
+            Fabricante? f = fabricantes[i];
+
+            if (f == null)
+            {
+                continue;
+            }
+
+            if (f.id == idSelecionado)
+            {
+                fabricantes[i] = null;
+                return true;
+            }
+
+        }
         return false;
     }
 }
+
